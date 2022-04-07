00865da8cc1c3d88714a8315ed8d000863cc5f18
RandyTsai <61904300+RandyTsai@users.noreply.github.com>
2022-04-07 17:21:19 +0800

    Add files via upload

---
+++ _lang/en-us.php
@@ -0,0 +1,8 @@
+<?php
+$_lang['themobiusation']['20900845961d3c1b178c78']	= 'Works';
+$_lang['themobiusation']['74527210061d3c1b179078']	= 'Menu';
+$_lang['themobiusation']['63111856761d3d2c4b917c']	= 'PREV';
+$_lang['themobiusation']['129890737461d3d2c4b9183']	= 'NEXT';
+$_lang['themobiusation']['100898359161d3d36d97894']	= 'Projects';
+
+?>
---
+++ _lang/zh-tw.php
@@ -0,0 +1,8 @@
+<?php
+$_lang['themobiusation']['20900845961d3c1b178c78']	= '專案';
+$_lang['themobiusation']['74527210061d3c1b179078']	= '目錄';
+$_lang['themobiusation']['63111856761d3d2c4b917c']	= '上一專案';
+$_lang['themobiusation']['129890737461d3d2c4b9183']	= '下一專案';
+$_lang['themobiusation']['100898359161d3d36d97894']	= '專案項目';
+
+?>
